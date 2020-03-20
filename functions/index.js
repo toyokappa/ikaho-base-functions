1f27b3b68978ce53a656d9832f95c4f38d216dac
Yuta Toyokawa <kppg42@gmail.com>
2020-03-20 17:51:06 +0900

    mail send setting

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,38 @@
 const functions = require('firebase-functions');
+const nodemailer = require('nodemailer')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const mailTransport = nodemailer.createTransport({
+  host: 'smpt.sendgrid.net',
+  port: 587,
+  requiresAuth: true,
+  auth: {
+    user: '',
+    pass: ''
+  }
+})
+
+const mailTemplate = (data) => {
+  return `以下の内容でホームページよりお問い合わせがありました。
+
+【お名前】 ${data.name}
+【連絡先】 ${data.email}
+【内　容】
+ ${data.message} 
+  `
+}
+
+exports.sendMail = functions.https.onCall(async (data, _context) => {
+  const mail = {
+    from: '',
+    to: '',
+    subject: '【伊香保BASE】お問い合わせがありました',
+    text: mailTemplate(data)
+  }
+
+  try {
+    await mailTransport.sendMail(mail)
+  } catch (e) {
+    console.error(`send faild: ${e}`)
+    throw new functions.https.HttpsError('internal', `send faild: ${e}`)
+  }
+})
